870140dc8a12ae6825a096f032f68eb5cfa9f29f
Jan Hybs <jan.hybs@tul.cz>
2019-04-10 18:16:32 +0200

    Add missing comma

--- www/static/js/core.js
+++ www/static/js/core.js
@@ -6,7 +6,7 @@ var Automatest = (function() {
   var callback = null;
   var URL = location.protocol + '//' + document.domain + ':' + location.port + namespace
   // https://socket.io/docs/client-api/
-  var socket = io.connect(URL {reconnection: false, upgrade: false, transports: ['websocket']});
+  var socket = io.connect(URL, {reconnection: false, upgrade: false, transports: ['websocket']});
   var env = nunjucks.configure(URL, {
     autoescape: true
   });
