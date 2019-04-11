aea3ae14e6a9e9e0ee6b64b077f4b5025e633653
Jan Hybs <jan.hybs@tul.cz>
2019-04-11 18:03:08 +0200

    Upgrade autopull

--- src/www/autopull.py
+++ src/www/autopull.py
@@ -3,16 +3,25 @@
 
 import subprocess
 
+from loguru import logger
 from flask import Flask
 from env import Env
 import sys
 
 src = Env.root / 'src'
 cmd = [sys.executable, 'www/start.py']
+app = Flask(__name__)
 
 
 class Glob():
     process = None
+
+
+def restart_server():
+    if Glob.process:
+        Glob.process.kill()
+
+    Glob.process = subprocess.Popen(cmd, cwd=str(src))
 
 
 def update_repo():
@@ -20,31 +29,64 @@ def update_repo():
         'git fetch --all',
         'git reset --hard origin/master',
     ]
-    result = None
+    result = 0
+    logger.info('updating main repo')
     for cmd in cmds:
         result = subprocess.Popen(cmd.split(), cwd=str(Env.root)).wait()
         print(cmd, result)
-        if result != 0:
-            sys.exit(1)
-    return result
+    return result == 0
 
+
+def update_subrepos():
+    result = 0
+    for course in Env.courses.glob('*'):
+        if course.is_dir():
+            cwd = str(course)
+            cmds = [
+                'git fetch --all',
+                'git reset --hard origin/master',
+            ]
+            logger.info('updating submodule {}', cwd)
+            for cmd in cmds:
+                result = subprocess.Popen(cmd.split(), cwd=cwd).wait()
+                print(cmd, result)
+    return result == 0
+
+
+@app.route('/webhook', methods=['POST', 'GET'])
+def webhook():
+    result = update_subrepos()
+
+    if not result:
+        return 'failed, {}'.format(result)
+
+    return 'ok, pull result = {}'.format(result)
 
 
 app = Flask(__name__)
-@app.route('/webhook', methods=['POST', 'GET'])
+@app.route('/webhook/full', methods=['POST', 'GET'])
 def webhook():
     if Glob.process:
         Glob.process.kill()
 
     result = update_repo()
+    if not result:
+        restart_server()
+        return 'failed, {}'.format(result)
+
+    result = update_subrepos()
+    if not result:
+        restart_server()
+        return 'failed, {}'.format(result)
+
     Glob.process = subprocess.Popen(cmd, cwd=str(src))
     return 'ok, pull result = {}'.format(result)
 
 
 
-
 update_repo()
-Glob.process = subprocess.Popen(cmd, cwd=str(src))
+update_subrepos()
+restart_server()
 
 # http://hybs.nti.tul.cz:5001/webhook
 app.run(host='0.0.0.0', port=5001)
