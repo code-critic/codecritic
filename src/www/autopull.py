9fceae2a8be41438afffd97c7b1d7b955bea5637
Jan Hybs <jan.hybs@tul.cz>
2019-04-11 12:51:25 +0200

    Change autopull

--- src/www/autopull.py
+++ src/www/autopull.py
@@ -11,18 +11,23 @@ src = Env.root / 'src'
 cmd = [sys.executable, 'www/start.py']
 
 
-update_repo = [
-    'git fetch --all'
-    'git reset --hard origin/master'
-]
-
-
-
 class Glob():
     process = None
 
 
-Glob.process = subprocess.Popen(cmd, cwd=str(src))
+def update_repo():
+    cmds = [
+        'git fetch --all',
+        'git reset --hard origin/master',
+    ]
+    result = None
+    for cmd in cmds:
+        result = subprocess.Popen(cmd.split(), cwd=str(Env.root)).wait()
+        print(cmd, result)
+        if result != 0:
+            sys.exit(1)
+    return result
+
 
 
 app = Flask(__name__)
@@ -31,16 +36,15 @@ def webhook():
     if Glob.process:
         Glob.process.kill()
 
-    result = None
-    for reset_cmd in update_repo:
-        result = subprocess.Popen(reset_cmd.split(), cwd=str(Env.root)).wait()
-        print(reset_cmd, result)
-        if result != 0:
-            sys.exit(1)
-
+    result = update_repo()
     Glob.process = subprocess.Popen(cmd, cwd=str(src))
     return 'ok, pull result = {}'.format(result)
 
+
+
+
+update_repo()
+Glob.process = subprocess.Popen(cmd, cwd=str(src))
 
 # http://hybs.nti.tul.cz:5001/webhook
 app.run(host='0.0.0.0', port=5001)
