d4df19c9e3889a9ff95f238a6859f4a10183928e
Jan Hybs <jan.hybs@tul.cz>
2019-04-17 19:48:10 +0200

    Add server setup test

--- tests/www/test_www.py
+++ tests/www/test_www.py
@@ -9,9 +9,21 @@ case = unittest.TestCase('__init__')
 
 
 @pytest.mark.www
-def test_server():
+def test_parser():
     args = start.parse_args(['-h', '0.0.0.0'])
 
     case.assertEqual(args.port, 5000)
     case.assertEqual(args.host, '0.0.0.0')
     case.assertIs(args.backdoor, False)
+
+
+@pytest.mark.www
+def test_server():
+    from www import app
+    from flask_socketio import SocketIO
+    import flask
+
+    async_mode = 'threading'
+    socketio = SocketIO(app, json=flask.json, async_mode=async_mode, ping_interval=100 * 1000)
+    start.register_routes(app, socketio)
+    
