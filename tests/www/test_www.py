ea8a818b92986355b55ab75402aaf868a5272f54
Jan Hybs <jan.hybs@tul.cz>
2019-04-18 09:52:58 +0200

    Add more tests

--- tests/www/test_www.py
+++ tests/www/test_www.py
@@ -3,6 +3,7 @@
 
 import pytest
 import unittest
+import os
 from www import start
 
 case = unittest.TestCase('__init__')
@@ -28,4 +29,28 @@ def test_server():
 
     async_mode = 'threading'
     socketio = SocketIO(app, json=flask.json, async_mode=async_mode, ping_interval=100 * 1000)
+
+    from www import backdoor
+    backdoor.register_routes(app, socketio)
     start.register_routes(app, socketio)
+
+
+@pytest.mark.www
+def test_remove_old():
+    from env import Env
+    from loguru import logger
+
+    info = '\n'.join(['{:>20s}: {:s}'.format(k, str(v)) for k, v in Env.info()])
+    logger.info('Configuration in env.py:\n{}', info)
+    logger.info('removing old files from {}', Env.tmp)
+
+    from utils import io
+    foo = Env.tmp.joinpath('foo')
+    foo.mkdir(parents=True, exist_ok=True)
+    bar = foo.joinpath('bar.txt')
+    bar.touch()
+
+    case.assertEqual(io.delete_old_files(Env.tmp, io.ONE_DAY), 0)
+
+    os.utime(str(foo), (0, 0))
+    case.assertEqual(io.delete_old_files(Env.tmp, io.ONE_DAY), 1)
