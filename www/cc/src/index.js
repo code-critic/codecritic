ad4b83cfe1fac1eb86afe8275ea45fb2c078b71b
Jan Hybs <jan.hybs@tul.cz>
2019-06-10 16:13:28 +0200

    Add webpack bundle

---
+++ www/cc/src/index.js
@@ -0,0 +1,10 @@
+import './css/anim.scss';
+import './css/colors.scss';
+import './css/colors.new.scss';
+import './css/icons.scss';
+import './css/new.scss';
+import './css/utils.scss';
+import './css/main.css';
+
+function App() {};
+export default App;
